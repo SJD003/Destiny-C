f374c077aea6d339c5cb224d4a6b110313fa2b27
Organization Of Destiny <72284630+Sajd003@users.noreply.github.com>
2021-06-26 14:23:37 +0300

    Update mod.js

--- mod.js
+++ mod.js
@@ -4,11 +4,27 @@ showSlides();
 function showSlides() {
     var i;
     var slides = document.getElementsByClassName("mySlides");
+    var dots = document.getElementsByClassName("dot");
     for (i = 0; i < slides.length; i++) {
         slides[i].style.display = "none";
     }
     slideIndex++;
     if (slideIndex > slides.length) { slideIndex = 1 }
+    for (i = 0; i < dots.length; i++) {
+        dots[i].className = dots[i].className.replace(" active", "");
+    }
     slides[slideIndex - 1].style.display = "block";
+    dots[slideIndex - 1].className += " active";
     setTimeout(showSlides, 5000); // Change image every 5 seconds
+}
+// Copyright© 2010 - 2021 Sajid Mottar Outaiwi - All rights reserved.
+
+
+// Copyright© 2010 - 2021 Sajid Mottar Outaiwi - All rights reserved.
+function openNav() {
+    document.getElementById("myNav").style.height = "100%";
+}
+
+function closeNav() {
+    document.getElementById("myNav").style.height = "0%";
 }
